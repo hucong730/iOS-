c22a7425d71b2a0a0d63b86838c39fd618de6b25
hucong <wddyzzw@icloud.com>
2017-02-08 17:25:23 +0800

    Init project

---
+++ CommonQuestion/CommonQuestion/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CommonQuestion
+//
+//  Created by 胡聪 on 2017/2/8.
+//  Copyright © 2017年 hucong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
